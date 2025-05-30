f76074e8b498672900a387134cfd48ad30027ef4
Toby Ward <toby@tbgtech.io>
2025-05-30 16:18:34 +0100

    remove project from nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,7 +68,7 @@ function NavBar() {
                 <AiOutlineUser style={{ marginBottom: "2px" }} /> About
               </Nav.Link>
             </Nav.Item>
-
+/*
             <Nav.Item>
               <Nav.Link
                 as={Link}
@@ -81,7 +81,7 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item>
-
+*/
             <Nav.Item>
               <Nav.Link
                 as={Link}
